e7a4b96db2c0d1a5f90455b872d971e3d7a8d372
Kennet Calixto <kennet.emerson@gmail.com>
2021-01-04 03:33:31 -0300

    adding weight for implementing dijkstra

--- data-structure/graph.c
+++ data-structure/graph.c
@@ -9,17 +9,20 @@
 
 typedef struct node{
   int vertex;
+  int weight;
   struct node * next;  
 }NODE;
 
 typedef struct graph{
-  int vertexCount;
+  int vertexCount;  
+  int * visited;
   NODE ** adjList;
 }GRAPH;
 
-NODE * create_node(int vertex){
+NODE * create_node(int vertex, int weight){
   NODE * tmp = (NODE * ) malloc (sizeof ( NODE ) );
   (*tmp).vertex = vertex;
+  (*tmp).weight = weight;
   (*tmp).next = NULL;
   return tmp;
 }
@@ -37,16 +40,17 @@ GRAPH * create_graph(int vertexCount){
   return graph;
 }
 
-void insert_edge(GRAPH * graph, int src, int dest){
+void insert_edge(GRAPH * graph, int src, int dest, int weight){
   //add edge from src to dest
-  NODE * tmp = create_node(dest);
+  NODE * tmp = create_node(dest, weight);
   (*tmp).next = (*graph).adjList[src];
   (*graph).adjList[src] = tmp;
 
   //add edge from dest to src
-  tmp = create_node(src);
-  (*tmp).next = (*graph).adjList[dest];
-  (*graph).adjList[dest] = tmp;
+
+  // tmp = create_node(src, weight);
+  // (*tmp).next = (*graph).adjList[dest];
+  // (*graph).adjList[dest] = tmp;
 }
 
 void print_graph(GRAPH * graph){
@@ -55,22 +59,25 @@ void print_graph(GRAPH * graph){
     NODE * tmp = (*graph).adjList[i];
     printf("\nvertex %d:\n", i);
     while (tmp != NULL){    
-      printf("%d -> ", (*tmp).vertex);
+      printf("[w:%d] %d -> ", (*tmp).weight, (*tmp).vertex);
       tmp = (*tmp).next;
     }    
   }
 }
 
+
 int main(){	  
 
-	GRAPH * graph = create_graph(4);
+	GRAPH * graph = create_graph(3);
 
-  insert_edge(graph, 0, 1);
-  insert_edge(graph, 0, 2);
-  insert_edge(graph, 0, 3);
-  insert_edge(graph, 1, 2);
+  insert_edge(graph, 0, 1, 1);
+  
+  //insert_edge(graph, 0, 2);
+  //insert_edge(graph, 0, 3);
+  //insert_edge(graph, 1, 2);
 
   print_graph(graph);
+  
 
 	return 0;
 }
