6029eebb68bc81cea0921ff381b98b862b8d6177
halkernel <kennet.emerson@gmail.com>
2021-04-24 04:57:01 -0300

    removing huffman print and starting minmax

---
+++ search-strategies/minmax.py
@@ -0,0 +1,37 @@
+from node import Node
+
+board = ['','','',
+         '','',''
+         '','','']
+
+pieces = ['X','O']
+
+visited = set()
+to_visit = list()
+current_node = Node
+
+
+def isFinished(node):
+    pass
+
+
+
+
+
+def expand():
+    pass
+
+
+
+player = input('O or X: ');
+computer = 'X' if player == 'O' else 'O'
+print('Computer will play with: ' + computer)
+who_goes_first = input('Who goes first?\n1: Computer | 2: Me: ')
+
+
+
+
+
+
+
+
